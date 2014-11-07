fca5e7a01753e6ef16e0b8473bb7501355da0ad5
Sjoekoe <hansjonckers@MacBook-Air-van-Hans.local>
2014-11-07 09:35:14 +0100

    added dockblocks

--- app/EDM/Roles/Role.php
+++ app/EDM/Roles/Role.php
@@ -3,8 +3,19 @@
 use Eloquent;
 
 class Role extends Eloquent {
+
+    /**
+     * The pivot table used for roles
+     *
+     * @var string
+     */
     protected $table = 'roles';
 
+    /**
+     * The fillable fields
+     * 
+     * @var array
+     */
     protected $fillable = ['user_id', 'role_id'];
 
 
