d38f5443a62d7d655128190175b861cea7a3f7ec
Sjoekoe <hansjonckers@MacBook-Air-van-Hans.local>
2014-11-07 09:28:50 +0100

    added dockblocks

--- app/EDM/Files/FilesRepository.php
+++ app/EDM/Files/FilesRepository.php
@@ -5,12 +5,20 @@ use Illuminate\Support\Facades\File;
 
 class FilesRepository {
 
+    /**
+     * Handle an image and persist it to the storage
+     * 
+     * @param $file
+     * @param $model
+     * @param $id
+     *
+     * @return mixed
+     */
     public function handleImage($file, $model, $id) {
 
         $extension  = $file->getClientOriginalExtension();
         $path       = '/images/uploads/' . $model . '/' . $id;
         $filename   = str_random(12);
-        //$pathToFile = public_path().$path . '/' . $filename . '.' . $extension;
 
         File::makeDirectory(public_path().$path, 0775, true);
 
